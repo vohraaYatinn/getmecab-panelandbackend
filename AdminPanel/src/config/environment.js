7620974068f1ebc204bd6671b5f565613332ea9d
yatin vohra <vohrayatin9@gmail.com>
2025-06-13 10:02:41 +0530

    ok

--- AdminPanel/src/config/environment.js
+++ AdminPanel/src/config/environment.js
@@ -1,5 +1,8 @@
 // export const test_url = 'http://api.chermulgold.com:8000/';
 // export const test_url_images = 'http://api.chermulgold.com:8000';
 
-export const test_url = 'http://127.0.0.1:8000/';
-export const test_url_images = 'http://127.0.0.1:8000';
+// export const test_url = 'http://127.0.0.1:8000/';
+// export const test_url_images = 'http://127.0.0.1:8000';
+
+export const test_url = 'http://69.62.75.228:8000/';
+export const test_url_images = 'http://69.62.75.228:8000';
